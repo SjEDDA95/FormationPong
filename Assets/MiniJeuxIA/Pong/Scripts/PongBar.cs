11bb824ee5708779f8c2ed0529db46d86d1a68b0
Skander JEDDA <105787181+SjEDDA95@users.noreply.github.com>
2024-01-11 15:28:48 +0100

    Initial commit

---
+++ Assets/MiniJeuxIA/Pong/Scripts/PongBar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongBar : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        
+    }
+}
